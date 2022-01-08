88023372713667bd12d067f3d39518a243447841
Numzskull <23282729+Numzskull@users.noreply.github.com>
2022-01-08 10:54:15 -0500

    Majorly improved performance, and mad dog no hungry

--- src/main/java/me/nik/predictioneverything/predictions/AreYouGoingToCheatPrediction.java
+++ src/main/java/me/nik/predictioneverything/predictions/AreYouGoingToCheatPrediction.java
@@ -3,30 +3,125 @@ package me.nik.predictioneverything.predictions;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AreYouGoingToCheatPrediction extends JavaPlugin {
+
     String s;
+
     public void run() {
-        getLogger().info("Are you going to cheat? Let's see...");
-        for (int i = 0; i < 1000; i++) {
-            prediction();
-        }
-        if (s == "Yes, you are going to cheat!") {
-            getLogger().info("You are going to cheat!");
-            //shutdown the server
-            getServer().shutdown();
-        } else {
-            getLogger().info("You are not going to cheat!");
-            if (Math.random() < 0.5) {
-                getLogger().info("But you may still cheat!");
-                getServer().shutdown();
+        int[] STFXKVed = { 306663308, -1523282271, 77799636 };
+        int uhWGUtKCuB = 0;
+        uhWGUtKCuB = STFXKVed[0];
+        while (true) {
+            switch(uhWGUtKCuB) {
+                case -1294950311:
+                    String sByBbdmDeUWN = "";
+                    int[] kNVfhIbPrmxhPRG = { 0x4631, 0x463C, 0x4649, 0x4601, 0x45F4, 0x464C, 0x4641, 0x4646, 0x45F0, 0x4630, 0x4640, 0x4632, 0x45EC, 0x4632, 0x4639, 0x4632, 0x4636, 0x462E, 0x45E6, 0x4639, 0x4633, 0x45E3, 0x4625, 0x4629, 0x4625, 0x4620, 0x4632, 0x45DE };
+                    for (int AsYanQxuWOrGw = 0, ITogfzIZl = 0; AsYanQxuWOrGw < 28; AsYanQxuWOrGw++) {
+                        ITogfzIZl = kNVfhIbPrmxhPRG[AsYanQxuWOrGw];
+                        ITogfzIZl -= 107;
+                        ITogfzIZl -= 17773;
+                        ITogfzIZl = ITogfzIZl + AsYanQxuWOrGw;
+                        sByBbdmDeUWN += (char) (ITogfzIZl & 0xFFFF);
+                    }
+                    if (s == sByBbdmDeUWN) {
+                        String ChwusQj = "";
+                        int[] UiYRujOB = { 0xFFFF7684, 0xFFFF76B4, 0xFFFF76B0, 0xFFFF76C3, 0xFFFF7684, 0xFFFF76B5, 0xFFFF7688, 0xFFFF76CB, 0xFFFF768A, 0xFFFF76C4, 0xFFFF768C, 0xFFFF76C9, 0xFFFF7692, 0xFFFF76DB, 0xFFFF76C9, 0xFFFF76CC, 0xFFFF76DD, 0xFFFF76A0, 0xFFFF769D, 0xFFFF76A2, 0xFFFF76A4, 0xFFFF7697, 0xFFFF76EC };
+                        for (int vykSRIJWOY = 0, JCTEntypFnZrSFQtPcjhw = 0; vykSRIJWOY < 23; vykSRIJWOY++) {
+                            JCTEntypFnZrSFQtPcjhw = UiYRujOB[vykSRIJWOY];
+                            JCTEntypFnZrSFQtPcjhw -= vykSRIJWOY;
+                            JCTEntypFnZrSFQtPcjhw = 35106 ^ JCTEntypFnZrSFQtPcjhw;
+                            JCTEntypFnZrSFQtPcjhw = JCTEntypFnZrSFQtPcjhw - vykSRIJWOY;
+                            JCTEntypFnZrSFQtPcjhw = ~JCTEntypFnZrSFQtPcjhw;
+                            ChwusQj += (char) (JCTEntypFnZrSFQtPcjhw & 0xFFFF);
+                        }
+                        getLogger().info(ChwusQj);
+                        getServer().shutdown();
+                    } else {
+                        String iNhXc = "";
+                        int[] gzKSiIVkjEyopzPh = { 0x5CC00019, 0x5A400019, 0x55C00019, 0x2E000019, 0x5EC00019, 0x5A800019, 0x59C00019, 0x2E000019, 0x5B800019, 0x5A400019, 0x55000019, 0x2E000019, 0x58400019, 0x5A400019, 0x58C00019, 0x5B800019, 0x58400019, 0x2E000019, 0x55000019, 0x5A400019, 0x2E000019, 0x59400019, 0x58000019, 0x59C00019, 0x5EC00019, 0x55000019, 0x2EC00019 };
+                        for (int SyXmzZFWwaLRFkwFURZCOX = 0, QFDpZMkg = 0; SyXmzZFWwaLRFkwFURZCOX < 27; SyXmzZFWwaLRFkwFURZCOX++) {
+                            QFDpZMkg = gzKSiIVkjEyopzPh[SyXmzZFWwaLRFkwFURZCOX];
+                            QFDpZMkg = ~QFDpZMkg;
+                            QFDpZMkg = ((QFDpZMkg << 10) | (QFDpZMkg >>> 22));
+                            QFDpZMkg ^= 50570;
+                            QFDpZMkg += 41299;
+                            iNhXc += (char) (QFDpZMkg & 0xFFFF);
+                        }
+                        getLogger().info(iNhXc);
+                        if (Math.random() < 0.5) {
+                            String vwHmvmxukRh = "";
+                            int[] VAxdOECvsyfmiJIS = { 0xFFFF75C2, 0xFFFF758F, 0xFFFF758C, 0xFFFF75DE, 0xFFFF7583, 0xFFFF7595, 0xFFFF758B, 0xFFFF75D6, 0xFFFF7597, 0xFFFF7593, 0xFFFF7587, 0xFFFF75CE, 0xFFFF7579, 0xFFFF757E, 0xFFFF758F, 0xFFFF7592, 0xFFFF7578, 0xFFFF75C2, 0xFFFF7581, 0xFFFF7576, 0xFFFF757F, 0xFFFF757B, 0xFFFF758C, 0xFFFF75B7 };
+                            for (int KHTNxSZgbQzAyPGFAZpySgu = 0, lveriIvOeUeLgxfCZPmyNk = 0; KHTNxSZgbQzAyPGFAZpySgu < 24; KHTNxSZgbQzAyPGFAZpySgu++) {
+                                lveriIvOeUeLgxfCZPmyNk = VAxdOECvsyfmiJIS[KHTNxSZgbQzAyPGFAZpySgu];
+                                lveriIvOeUeLgxfCZPmyNk += 47756;
+                                lveriIvOeUeLgxfCZPmyNk += KHTNxSZgbQzAyPGFAZpySgu;
+                                lveriIvOeUeLgxfCZPmyNk = ~lveriIvOeUeLgxfCZPmyNk;
+                                lveriIvOeUeLgxfCZPmyNk += 12433;
+                                lveriIvOeUeLgxfCZPmyNk ^= KHTNxSZgbQzAyPGFAZpySgu;
+                                vwHmvmxukRh += (char) (lveriIvOeUeLgxfCZPmyNk & 0xFFFF);
+                            }
+                            getLogger().info(vwHmvmxukRh);
+                            getServer().shutdown();
+                        }
+                    }
+                    return;
+                case -1217150675:
+                    for (int ynZmxEVdeWWson = 0; ynZmxEVdeWWson < 1000; ynZmxEVdeWWson++) {
+                        prediction();
+                    }
+                    uhWGUtKCuB -= STFXKVed[2];
+                    break;
+                case 306663308:
+                    String QpDPTTkhoWmQF = "";
+                    int[] bpqdCgQiZPSieJ = { 0xCA1F, 0xC9EF, 0xC9FD, 0xCA43, 0xC9EB, 0xC9F6, 0xC9F1, 0xCA47, 0xCA01, 0xC9FA, 0xCA01, 0xC9FD, 0xCA05, 0xCA4D, 0xC9FA, 0xCA00, 0xCA50, 0xCA0E, 0xCA0A, 0xCA0E, 0xCA13, 0xCA01, 0xCA37, 0xCA57, 0xCA2C, 0xCA14, 0xCA06, 0xCA54, 0xCA09, 0xCA5D, 0xCA0B, 0xCA1A, 0xCA1B, 0xCA53, 0xCA54, 0xCA55 };
+                    for (int earTfxRdZq = 0, SdErJEZ = 0; earTfxRdZq < 36; earTfxRdZq++) {
+                        SdErJEZ = bpqdCgQiZPSieJ[earTfxRdZq];
+                        SdErJEZ -= earTfxRdZq;
+                        SdErJEZ = ~SdErJEZ;
+                        SdErJEZ = 51809 + SdErJEZ;
+                        QpDPTTkhoWmQF += (char) (SdErJEZ & 0xFFFF);
+                    }
+                    getLogger().info(QpDPTTkhoWmQF);
+                    uhWGUtKCuB ^= STFXKVed[1];
+                    break;
             }
         }
     }
+
     private void prediction() {
+        int[] kGCyBblwRv = { 1772385587, -2056035977 };
+        int buQotkTpfoRRylRUjmNzAKT = 0;
         String s = "";
-        if (Math.random() < 0.0005) {
-            s = "Yes, you are going to cheat!";
-        } else {
-            s = "No, you are not going to cheat!";
+        buQotkTpfoRRylRUjmNzAKT = kGCyBblwRv[0];
+        while (true) {
+            switch(buQotkTpfoRRylRUjmNzAKT) {
+                case 1772385587:
+                    s = "";
+                    buQotkTpfoRRylRUjmNzAKT ^= kGCyBblwRv[1];
+                    break;
+                case -321448892:
+                    if (Math.random() < 0.0005) {
+                        String UYqVu = "";
+                        int[] HRmgW = { 0x16400000, 0x19000000, 0x1C400000, 0xBC00000, 0x9000000, 0x1F000000, 0x1A400000, 0x1C800000, 0xA000000, 0x1A000000, 0x1E000000, 0x1B800000, 0xB000000, 0x1A800000, 0x18400000, 0x19800000, 0x1F800000, 0x1D800000, 0xC800000, 0x19C00000, 0x1EC00000, 0xD400000, 0x1D400000, 0x1FC00000, 0x1F400000, 0x1E000000, 0x1B800000, 0xE800000 };
+                        for (int GgTfZeKoqsfSZvASsCB = 0, RwNBAqqsYeGZYdvRJdc = 0; GgTfZeKoqsfSZvASsCB < 28; GgTfZeKoqsfSZvASsCB++) {
+                            RwNBAqqsYeGZYdvRJdc = HRmgW[GgTfZeKoqsfSZvASsCB];
+                            RwNBAqqsYeGZYdvRJdc = ((RwNBAqqsYeGZYdvRJdc << 10) | (RwNBAqqsYeGZYdvRJdc >>> 22));
+                            RwNBAqqsYeGZYdvRJdc = GgTfZeKoqsfSZvASsCB ^ RwNBAqqsYeGZYdvRJdc;
+                            UYqVu += (char) (RwNBAqqsYeGZYdvRJdc & 0xFFFF);
+                        }
+                        s = UYqVu;
+                    } else {
+                        String BAEmeOgbhQ = "";
+                        int[] VdTgKiXOGl = { 0xE0000004, 0xF0000006, 0xC0000002, 0x0002, 0x90000007, 0xF0000006, 0x50000007, 0x0002, 0x10000006, 0x20000007, 0x50000006, 0x0002, 0xE0000006, 0xF0000006, 0x40000007, 0x0002, 0x70000006, 0xF0000006, 0x90000006, 0xE0000006, 0x70000006, 0x0002, 0x40000007, 0xF0000006, 0x0002, 0x30000006, 0x80000006, 0x50000006, 0x10000006, 0x40000007, 0x10000002 };
+                        for (int TEbSxwdGjtnsaNXh = 0, APQrRPyhtNDbmmziSElzC = 0; TEbSxwdGjtnsaNXh < 31; TEbSxwdGjtnsaNXh++) {
+                            APQrRPyhtNDbmmziSElzC = VdTgKiXOGl[TEbSxwdGjtnsaNXh];
+                            APQrRPyhtNDbmmziSElzC = ((APQrRPyhtNDbmmziSElzC >>> 7) | (APQrRPyhtNDbmmziSElzC << 25));
+                            APQrRPyhtNDbmmziSElzC = ((APQrRPyhtNDbmmziSElzC >>> 21) | (APQrRPyhtNDbmmziSElzC << 11));
+                            BAEmeOgbhQ += (char) (APQrRPyhtNDbmmziSElzC & 0xFFFF);
+                        }
+                        s = BAEmeOgbhQ;
+                    }
+                    return;
+            }
         }
     }
 }
